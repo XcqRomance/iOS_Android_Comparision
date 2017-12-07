5f85e5667e1c175856bf65b6152b15a86d981ed3
XcqRomance <755412806@qq.com>
2017-12-07 16:59:05 +0800

    PlayVideoTest/.idea/misc.xml

---
+++ NetworkTest/app/src/main/java/com/example/romance/networktest/Test.java
@@ -0,0 +1,31 @@
+package com.example.romance.networktest;
+
+/**
+ * Created by xiechuanquan on 2017/12/7.
+ */
+
+public class Test {
+    /**
+     * name : tom
+     * age : 30
+     */
+
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
